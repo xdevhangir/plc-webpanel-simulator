9a0615af3cc9d3a104282e6b38783dee63a11f8a
xdevhangir <chnaladogan@outlook.com>
2025-05-16 01:35:05 +0300

    WebPanelApp and WebPanelTest is added

---
+++ WebPanelApp/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    status = "Bağlantı Yok"
+    veri = ""
+    if request.method == 'POST':
+        status = "Bağlı"
+        veri = request.form.get('giris_verisi')
+    return render_template('index.html', status=status, veri=veri)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ WebPanelTest/WebPanelTest/Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WebPanelTests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless"); // arka planda çalıştırmak için
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void Test_PLC_Verisi_Gonder()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            var input = driver.FindElement(By.Id("giris"));
+            input.SendKeys("Deneme Verisi");
+
+            var button = driver.FindElement(By.XPath("//button"));
+            button.Click();
+
+            var sonuc = driver.FindElement(By.XPath("//p[strong[contains(text(),'Deneme Verisi')]]"));
+
+            if (!sonuc.Text.Contains("Deneme Verisi"))
+                Assert.Fail("Test Failed");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
